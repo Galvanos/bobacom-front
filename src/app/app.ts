a26d58b90697fcc10cdc9710d7b6d0430973c2c1
Luca Ongaro <48388970+Galvanos@users.noreply.github.com>
2026-07-21 12:21:23 +0200

    publish project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importante per usare *ngFor
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: 'app-root', // Questo nome deve essere identico al tag in index.html
+  standalone: true,
+  imports: [CommonModule], // Necessario per le direttive come *ngFor
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('bobacom-prova');
+export class AppComponent {
+  prodotti = [
+    { nome: 'Taro Milk Tea', prezzo: 4.50 },
+    { nome: 'Mango Green Tea', prezzo: 5.00 }
+  ];
 }

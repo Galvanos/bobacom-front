0d77952c3e515dabd460110263c3d4bb67704f91
Marco Scarpa <marco.scarpa@tiscali.it>
2026-07-22 18:02:32 +0200

    tentativo di creazione di pagina di login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -21,11 +21,13 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([authInterceptor])    // interceptor registration
     ),
+    /*
     provideAppInitializer(() => { // service to execute in startup
       const refreshService = inject(AuthNetworkService);
       return firstValueFrom(refreshService.restoreSession()) // execute refresh in startup
 
     }),
+    */
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration()
   ]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Login } from './dialogs/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:Login, pathMatch:'full'}
+];

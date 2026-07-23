ca7f66a783586abc72c4ca51c6e93bbb545862df
Marco Scarpa <marco.scarpa@tiscali.it>
2026-07-23 10:37:54 +0200

    Aggiunta pagina di login
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/dialogs/login/login.css
    	modified:   src/app/dialogs/login/login.html
    	modified:   src/app/dialogs/login/login.ts

--- src/app/dialogs/login/login.ts
+++ src/app/dialogs/login/login.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
+import { FormsModule, NgModel,NgForm } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { AuthNetworkService } from '../../security/auth-network-service';
+import { AuthService } from '../../auth/auth-service';
+import { Router } from '@angular/router';
+import { UtenteDTO } from '../../models/dto';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  msg = signal("");
+  @ViewChild('loginForm') loginForm!:NgForm;
+
+
+  constructor(private networkAuthService:AuthNetworkService,
+              private auth:AuthService,
+              private routing:Router
+  ){
+
+  }
+
+
+  onSubmit(){
+    this.networkAuthService.login({
+      username:this.loginForm.value.userName,
+      password:this.loginForm.value.password
+    }).subscribe({
+      next: ((r:UtenteDTO) => {
+        this.msg.set("");
+        this.auth.setAuthenticated(r)
+        this.routing.navigate(['']);
+      }),
+      error: ((r:any) => {
+        this.msg.set(r.error.msg);
+      })
+    })
+  }
+
+}

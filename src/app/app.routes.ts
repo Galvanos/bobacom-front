a90fb1890a86178c56b6a0619939d62f7962e6f5
Luca Ongaro <ongaro.lol.luca@gmail.com>
2026-07-27 19:33:59 +0200

    wip productMgmt page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { StockManagement } from './components/stock-management/stock-management';
 import { Home } from './components/home/home';
 import { Dashboard } from './components/dashboard/dashboard';
+import { IngredientsManagement } from './components/ingredients-management/ingredients-management';
+import { ProductManagement } from './components/product-management/product-management';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'dash', pathMatch: 'full'},
     {path: 'dash', component: Dashboard, children: [
             {path: 'home', component: Home},
-            {path: 'stock-management', component: StockManagement}
+            {path: 'stock-management', component: StockManagement},
+            {path: 'ingredients-management', component: IngredientsManagement},            
+            {path: 'product-management', component: ProductManagement}
     ]
     },
 ];
---
+++ src/app/components/ingredients-management/ingredients-management.ts
@@ -0,0 +1,66 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { FormGroup, FormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { IngredientsService } from '../../services/ingredients-service';
+import { CategoriaIngredienteService } from '../../services/categoria-ingrediente-service';
+import { AllergeniService } from '../../services/allergeni-service';
+
+@Component({
+  selector: 'app-stock-management',
+  imports: [MatCardModule, MatFormFieldModule, MatDividerModule, MatSelectModule, CommonModule, FormsModule],
+  templateUrl: './ingredients-management.html',
+  styleUrl: './ingredients-management.css',
+})
+export class IngredientsManagement implements OnInit{
+
+  private ingredienteService = inject(IngredientsService);
+  private categoriaIngredienteService = inject(CategoriaIngredienteService);
+  private allergeniService = inject(AllergeniService);
+
+  ingredienteSignal = this.ingredienteService.ingredients;
+  categoriaIngredienteSignal = this.categoriaIngredienteService.categorieIngredienti;
+  allergeniSignal = this.allergeniService.allergeni;
+
+  ngOnInit(): void {
+    this.ingredienteService.list();
+    this.categoriaIngredienteService.list();
+    this.allergeniService.list();
+  }
+
+  newIngrediente = {
+    nome: '',
+    descrizione: '',
+    quantitaStock: 0,
+    sovraprezzoAggiunta: 0.2,
+    prezzoRestock: 0.1,
+    colore: '',
+    idAllergene: [] as string[],
+    idCategoriaIngrediente: 0
+  };
+
+  onIngredienteSubmit(): void {
+    if(!this.newIngrediente.nome || !this.newIngrediente.idCategoriaIngrediente) {
+      return;
+    }
+
+    this.ingredienteService.create(this.newIngrediente).subscribe({
+      next: () => {
+        this.newIngrediente = {          
+          nome: '',
+          descrizione: '',
+          quantitaStock: 0,
+          sovraprezzoAggiunta: 0,
+          prezzoRestock: 0,
+          colore: '',
+          idAllergene: [] as string[],
+          idCategoriaIngrediente: 0
+        }
+      }
+    })
+  }
+
+}

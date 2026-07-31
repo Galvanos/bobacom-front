ad9cc10dfe0a843030d413ecbcc0a5453bf6ad15
Luca Ongaro <ongaro.lol.luca@gmail.com>
2026-07-31 17:51:07 +0200

    add empty tag mgmt page

---
+++ src/app/components/tag-management/tag-management.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-management',
+  imports: [],
+  templateUrl: './tag-management.html',
+  styleUrl: './tag-management.css',
+})
+export class TagManagement {}

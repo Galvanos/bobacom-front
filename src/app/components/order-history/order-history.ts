5c1247f45a4067c3f58724fda11edd8f58d4b06d
Luca Ongaro <ongaro.lol.luca@gmail.com>
2026-08-05 10:08:45 +0200

    fix order history display

--- src/app/components/order-history/order-history.ts
+++ src/app/components/order-history/order-history.ts
@@ -18,5 +18,6 @@ export class OrderHistory implements OnInit{
 
   ngOnInit(): void {
     this.ordineService.listByUser(this.thisAuthUser.userId!.toFixed(0));
+    console.log(this.ordineSignal()[0]);
   }
 }
